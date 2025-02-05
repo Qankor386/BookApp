6d5b35b264ef99f07ffcfc02bd163d1fa68ce305
Martin Berčík <m1_bercik@utb.cz>
2025-02-05 16:45:13 +0100

    Edit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,7 +25,20 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  title: { fontSize: 20, fontWeight: "bold" },
-  book: { fontSize: 18, marginVertical: 10 },
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center",
+    backgroundColor: "#121212",
+  },
+  title: { 
+    fontSize: 20, 
+    fontWeight: "bold", 
+    color: "#FFFFFF"
+  },
+  book: { 
+    fontSize: 18, 
+    marginVertical: 10, 
+    color: "#FFFFFF"
+  }
 });
