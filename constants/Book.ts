e6e8838bc7228dd4416eac0a4383d802eae2da2a
Martin Berčík <m1_bercik@utb.cz>
2025-02-05 17:21:25 +0100

    Definovani knihy

---
+++ constants/Book.ts
@@ -0,0 +1,9 @@
+export type Book = {
+    title: string;
+    series?: string;
+    author: string;
+    rating?: number;
+    review?: string;
+    releaseDate: string;
+    addedDate: string;
+  };
